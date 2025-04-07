93346b4ef0ee124d068b0eb7f9eeb75c2e9420a4
julien_inchausti <julien_inchausti@rubygroupe.jp>
2025-04-07 12:23:24 +0900

    Format files

--- src/components/ToiletResultTile.tsx
+++ src/components/ToiletResultTile.tsx
@@ -1,15 +1,19 @@
-import React from 'react';
+import React from "react";
 
-const ToiletResultTile = ({toilet}) => {
-    return (
-        <div className={"tileContainer fontBodyMedium"}>
-            <div className="title fontHeadlineSmall">{toilet.name} · {toilet.distance}km</div>
-            <div className="rating">{toilet.rating} ({toilet.ratingCount})</div>
-            <div className="typeAddress">
-                <span>{toilet.type}</span> · <span>{toilet.address}</span>
-            </div>
-        </div>
-    );
+const ToiletResultTile = ({ toilet }) => {
+  return (
+    <div className={"tileContainer fontBodyMedium"}>
+      <div className="title fontHeadlineSmall">
+        {toilet.name} · {toilet.distance}km
+      </div>
+      <div className="rating">
+        {toilet.rating} ({toilet.ratingCount})
+      </div>
+      <div className="typeAddress">
+        <span>{toilet.type}</span> · <span>{toilet.address}</span>
+      </div>
+    </div>
+  );
 };
 
 export default ToiletResultTile;

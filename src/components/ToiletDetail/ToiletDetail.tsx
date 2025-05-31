37d76d2e20402179399333a58e89fe1e224e7a4f
julien_inchausti <julien_inchausti@rubygroupe.jp>
2025-05-31 23:36:57 +0900

    Improving photo display in detail (still have a bug in full screen for loading)

--- src/components/ToiletDetail/ToiletDetail.tsx
+++ src/components/ToiletDetail/ToiletDetail.tsx
@@ -4,7 +4,7 @@ import "./ToiletDetail.css";
 import ReactStars from "react-rating-stars-component";
 import ImageGallery from "react-image-gallery";
 import "react-image-gallery/styles/css/image-gallery.css";
-import noPhotoAvailable from '../../assets/no-photo-available.jpg';
+import noPhotoAvailable from "../../assets/no-photo-available.jpg";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faXmark } from "@fortawesome/free-solid-svg-icons";
 
@@ -14,22 +14,32 @@ const ToiletDetail = ({
   setToiletDetails,
 }) => {
   const [photos, setPhotos] = useState([]);
-
+  const [isFullScreen, setIsFullScreen] = useState(false);
+  var currentPhotoIndex = useRef(0);
   const preparePhotos = async (toiletDetails) => {
     const photos = [];
     if (toiletDetails.photos && toiletDetails.photos.length > 0) {
       toiletDetails.photos.forEach((photo) => {
         photos.push({
           original: photo.uri,
-          
+          fullscreen: photo.uri,
         });
       });
     }
-    if (toiletDetails.googleMapsPhotos && toiletDetails.googleMapsPhotos.length > 0) {
+    if (
+      toiletDetails.googleMapsPhotos &&
+      toiletDetails.googleMapsPhotos.length > 0
+    ) {
       toiletDetails.googleMapsPhotos.forEach((photo) => {
         photos.push({
-          isToBeFetched: !photo.uri,
-          original: photo.uri ? photo.uri : "https://upload.wikimedia.org/wikipedia/commons/b/b1/Loading_icon.gif",
+          isToBeFetchedOriginal: !photo.uri,
+          isToBeFetchedFullscreen: !photo.uri,
+          original: photo.uri
+            ? photo.uri
+            : "https://upload.wikimedia.org/wikipedia/commons/b/b1/Loading_icon.gif",
+          fullscreen: photo.uri
+            ? photo.uri
+            : "https://upload.wikimedia.org/wikipedia/commons/b/b1/Loading_icon.gif",
           name: photo.name,
           description: photo.authorAttribution.userName,
           height: photo.height,
@@ -38,17 +48,22 @@ const ToiletDetail = ({
       });
     }
     // If first photo has no url and has a name (from google maps), fetch the url from google maps api
-    if (photos.length > 0 && photos[0].isToBeFetched && photos[0].name) {
+    if (
+      photos.length > 0 &&
+      photos[0].isToBeFetchedOriginal &&
+      photos[0].name
+    ) {
       const firstPhoto = photos[0];
       const photo = await fetchPhoto(firstPhoto.name, 400, 400);
       // Create photo url from content
-      firstPhoto.original = 'data:image/png;base64,' + photo;
-      firstPhoto.isToBeFetched = false;
+      firstPhoto.original = "data:image/png;base64," + photo;
+      firstPhoto.isToBeFetchedOriginal = false;
     }
     return photos;
-  }
-  
+  };
+
   useEffect(() => {
+    currentPhotoIndex.current = 0;
     const fetchPhotos = async () => {
       const preparedPhotos = await preparePhotos(toiletDetails);
       setPhotos(preparedPhotos);
@@ -56,29 +71,102 @@ const ToiletDetail = ({
     fetchPhotos();
   }, [toiletDetails]);
 
-
   const handleOnSlide = (index) => {
+    console.log("isFullScreen ?", isFullScreen.current);
     console.log("Slide changed to index:", index);
+    currentPhotoIndex.current = index;
     const currentPhoto = photos[index];
-    if (currentPhoto && currentPhoto.name && currentPhoto.isToBeFetched) {
+    if (
+      !isFullScreen.current &&
+      currentPhoto &&
+      currentPhoto.name &&
+      currentPhoto.isToBeFetchedOriginal
+    ) {
       fetchPhoto(currentPhoto.name, 400, 400)
         .then((content) => {
-          const photoUrl = 'data:image/png;base64,' + content;
+          const photoUrl = "data:image/png;base64," + content;
           const updatedPhotos = [...photos];
           updatedPhotos[index].original = photoUrl;
-          updatedPhotos[index].isToBeFetched = false;
+          updatedPhotos[index].isToBeFetchedOriginal = false;
           setPhotos(updatedPhotos);
         })
         .catch((error) => {
-          console.error("Error fetching photo URL:", error);
+          console.error("Error fetching original photo URL:", error);
+        });
+    } else if (
+      isFullScreen.current &&
+      currentPhoto &&
+      currentPhoto.name &&
+      currentPhoto.isToBeFetchedFullscreen
+    ) {
+      fetchPhoto(currentPhoto.name, currentPhoto.height, currentPhoto.width)
+        .then((content) => {
+          const photoUrl = "data:image/png;base64," + content;
+          const updatedPhotos = [...photos];
+          updatedPhotos[index].fullscreen = photoUrl;
+          updatedPhotos[index].isToBeFetchedFullscreen = false;
+          setPhotos(updatedPhotos);
+        })
+        .catch((error) => {
+          console.error("Error fetching fullscreen photo URL:", error);
         });
     }
   };
+
+  const handleOnScreenChange = (value) => {
+    setIsFullScreen(value);
+    console.log("Current photo index:", currentPhotoIndex.current);
+    const currentPhoto = photos[currentPhotoIndex.current];
+    console.log("Screen change to:", value);
+    console.log("Current photo:", currentPhoto);
+    if (
+      !isFullScreen.current &&
+      currentPhoto &&
+      currentPhoto.name &&
+      currentPhoto.isToBeFetchedOriginal
+    ) {
+      fetchPhoto(currentPhoto.name, 400, 400)
+        .then((content) => {
+          const photoUrl = "data:image/png;base64," + content;
+          const updatedPhotos = [...photos];
+          updatedPhotos[currentPhotoIndex.current].original = photoUrl;
+          updatedPhotos[currentPhotoIndex.current].isToBeFetchedOriginal =
+            false;
+          setPhotos(updatedPhotos);
+        })
+        .catch((error) => {
+          console.error("Error fetching original photo URL:", error);
+        });
+    } else if (
+      isFullScreen.current &&
+      currentPhoto &&
+      currentPhoto.name &&
+      currentPhoto.isToBeFetchedFullscreen
+    ) {
+      console.log("Fetching fullscreen photo for:", currentPhotoIndex.current);
+      fetchPhoto(currentPhoto.name, currentPhoto.height, currentPhoto.width)
+        .then((content) => {
+          const photoUrl = "data:image/png;base64," + content;
+          const updatedPhotos = [...photos];
+          updatedPhotos[currentPhotoIndex.current].fullscreen = photoUrl;
+          updatedPhotos[currentPhotoIndex.current].isToBeFetchedFullscreen =
+            false;
+          setPhotos(updatedPhotos);
+        })
+        .catch((error) => {
+          console.error("Error fetching fullscreen photo URL:", error);
+        });
+    }
+  };
+
   console.log("photos", photos);
-  const distance = toiletDetails.distance >= 1 ? toiletDetails.distance + 'km' : toiletDetails.distance * 1000 + 'm';
-  
+  const distance =
+    toiletDetails.distance >= 1
+      ? toiletDetails.distance + "km"
+      : toiletDetails.distance * 1000 + "m";
+
   return (
-    <>  
+    <>
       <div className={`detail-panel`}>
         <button
           className="detail-panel__close-button"
@@ -89,40 +177,52 @@ const ToiletDetail = ({
         >
           <FontAwesomeIcon icon={faXmark} />
         </button>
-        <div className="detail-panel__photos">
-          {photos && photos.length > 0 ? (        
-          <ImageGallery items={photos} onSlide={handleOnSlide} showBullets={true} showPlayButton={false} />
+        <div
+          className={`detail-panel__photos ${
+            isFullScreen ? "fullscreen" : "original"
+          }`}
+        >
+          {photos && photos.length > 0 ? (
+            <ImageGallery
+              items={photos}
+              onSlide={handleOnSlide}
+              showBullets={true}
+              showPlayButton={false}
+              onScreenChange={(value) => handleOnScreenChange(value)}
+              originalClass="detail-panel__photos original"
+            />
           ) : (
-          <img src={noPhotoAvailable} alt="No photo available" />
+            <img src={noPhotoAvailable} alt="No photo available" />
           )}
         </div>
         <div className="detail-panel__info">
           <div className="title fontHeadlineSmall">
-                  {toiletDetails.name} · {distance}
-                </div>
-                {toiletDetails.ratingCount > 0 ? (
-                  <div className="rating">
-                    <span className="ratingValue">{toiletDetails.rating}</span>
-                    <span className="ratingStars">
-                      <ReactStars
-                        key={toiletDetails.googlePlaceId}
-                        count={5}
-                        size={15}
-                        activeColor="#ffd700"
-                        edit={false}
-                        value={toiletDetails.rating}
-                        isHalf={true}
-                        classNames="ratingStars"
-                      />
-                    </span>
-                    <span className="ratingCount">({toiletDetails.ratingCount})</span>
-                  </div>
-                ) : (
-                  <div className="rating">No reviews yet</div>
-                )}
-                <div className="typeAddress">
-                  <span>{toiletDetails.type}</span> · <span>{toiletDetails.address}</span>
-                </div>
+            {toiletDetails.name} · {distance}
+          </div>
+          {toiletDetails.ratingCount > 0 ? (
+            <div className="rating">
+              <span className="ratingValue">{toiletDetails.rating}</span>
+              <span className="ratingStars">
+                <ReactStars
+                  key={toiletDetails.googlePlaceId}
+                  count={5}
+                  size={15}
+                  activeColor="#ffd700"
+                  edit={false}
+                  value={toiletDetails.rating}
+                  isHalf={true}
+                  classNames="ratingStars"
+                />
+              </span>
+              <span className="ratingCount">({toiletDetails.ratingCount})</span>
+            </div>
+          ) : (
+            <div className="rating">No reviews yet</div>
+          )}
+          <div className="typeAddress">
+            <span>{toiletDetails.type}</span> ·{" "}
+            <span>{toiletDetails.address}</span>
+          </div>
         </div>
       </div>
     </>
